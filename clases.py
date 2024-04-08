ffb558cffa0358819ec4c5863467eb69fb7b83f9
valepabon <125760572+valepabon@users.noreply.github.com>
2024-04-08 12:00:43 -0500

    Add files via upload

---
+++ clases.py
@@ -0,0 +1,21 @@
+class Usuario:
+    def _init_(self, rol, username):
+        self.user = username
+        self.rol = rol
+    def getUser(self):
+        return self.user
+    def getRol(self):
+        return self.rol
+    def setUser(self,user):
+        self.user = user
+    def setRol(self,rol):
+        self.rol= rol
+    def verUsuario(self):
+        print('El usuario es: ', self.user, ' con el rol de ', self.rol)
+        
+    
+
+
+user1 = Usuario('Administrador', 'Admin1')
+
+
