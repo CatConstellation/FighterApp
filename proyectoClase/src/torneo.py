8330471106a74192d3eea86336b76a6b9378692c
valepabon <125760572+valepabon@users.noreply.github.com>
2024-04-24 17:36:32 -0500

    Add files via upload

---
+++ proyectoClase/src/torneo.py
@@ -0,0 +1,8 @@
+class Torneo:
+    def __init__(self, ID, inscripcion, juego, premios, participantesNum, requisito):
+        self.ID = ID
+        self.inscripcion = inscripcion
+        self.juego = juego
+        self.premios = premios
+        self.participantesNum = participantesNum
+        self.requisito = requisito
