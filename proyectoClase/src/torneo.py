f9c41c43a4dcc917bed7d8c208fd8c8fa6ac8ea1
SpaceInBlank <91902169+SpaceInBlank@users.noreply.github.com>
2024-04-29 15:41:26 -0500

    Delete proyectoClase/src directory

--- proyectoClase/src/torneo.py
+++
@@ -1,8 +0,0 @@
-class Torneo:
-    def __init__(self, ID, inscripcion, juego, premios, participantesNum, requisito):
-        self.ID = ID
-        self.inscripcion = inscripcion
-        self.juego = juego
-        self.premios = premios
-        self.participantesNum = participantesNum
-        self.requisito = requisito
