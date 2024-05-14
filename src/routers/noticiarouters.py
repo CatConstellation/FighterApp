1f588a448d7fb2ffc03ccca310ccfae28cf2e763
HenryVQ197 <72803163+HenryVQ197@users.noreply.github.com>
2024-05-14 09:38:36 -0500

    Update noticiarouters.py
    Se agrego la funcion eliminarnoticia

--- src/routers/noticiarouters.py
+++ src/routers/noticiarouters.py
@@ -1,7 +1,9 @@
 from fastapi import HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from conexionbd import SessionLocal
-from src.models.noticiamodel import create_noticia as create_noticia_db, get_noticias as get_noticias_db, get_noticia as get_noticia_db
+from src.models.noticiamodel import create_noticia as create_noticia_db
+from src.models.noticiamodel import get_noticias as get_noticias_db, get_noticia as get_noticia_db
+from src.models.noticiamodel import eliminar_noticia as eliminar_noticia_db
 from src.models.schemas import NoticiaCreate, NoticiaSchema
 
 router = APIRouter()
@@ -13,11 +15,16 @@ def get_db():
     finally:
         db.close()
 
-
-
 @router.post("/noticias/", response_model=NoticiaSchema)
 def create_noticia(noticia: NoticiaCreate, db: Session = Depends(get_db)):
     return create_noticia_db(db, noticia)
+
+@router.delete("/noticias/{noticia_id}")
+def delete_noticia(noticia_id: int, db: Session = Depends(get_db)):
+    success = eliminar_noticia_db(db, noticia_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Noticia no encontrada")
+    return {"message": "Noticia eliminada correctamente"}
 
 @router.get("/noticias/", response_model=list[NoticiaSchema])
 def read_noticias(db: Session = Depends(get_db)):
