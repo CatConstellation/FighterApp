6c15f46ac307da462257ef55c6b5ecadeb8330c0
valentina <valentina.pabonl@urosario.edu.co>
2024-05-14 04:36:34 -0500

    Merge remote changes into proyectoVALENTINA

--- src/controllers/noticiacontroller.py
+++ src/controllers/noticiacontroller.py
@@ -1,14 +1,8 @@
 from fastapi import APIRouter
-from views.noticiaviews import show_noticias, show_create_noticia_form
+from src.models import get_crearnoticia
 
 router = APIRouter()
 
-@router.get("/noticias")
-async def get_noticias():
-    noticias = show_noticias()
-    return noticias
-
-@router.get("/noticias/crear")
-async def create_noticia_form():
-    return show_create_noticia_form()
-
+@router.get("/")
+async def get_index():
+    return get_crearnoticia()
