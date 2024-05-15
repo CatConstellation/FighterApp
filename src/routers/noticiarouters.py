a82d6d2334cfc8225381e8580a8c3e80523d580e
HenryVQ197 <72803163+HenryVQ197@users.noreply.github.com>
2024-05-15 08:43:06 -0500

    Update noticiarouters.py

--- src/routers/noticiarouters.py
+++ src/routers/noticiarouters.py
@@ -4,7 +4,6 @@ from conexionbd import SessionLocal
 from src.models.noticiamodel import get_noticias as get_noticias_db, get_noticia as get_noticia_db
 from src.models.noticiamodel import eliminar_noticia as eliminar_noticia_db
 from src.models.schemas import NoticiaDelete, NoticiaSchema
-from src.models.noticiamodel import eliminar_noticia
 
 router = APIRouter()
 
@@ -14,23 +13,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-@router.delete("/noticias/{noticia_id}")
-def delete_noticia(noticia_id: int, db: Session = Depends(get_db)):
-    """Deletes a noticia from the database by its ID.
-
-    Args:
-        noticia_id: ID of the noticia to be deleted.
-        db: SQLAlchemy session object.
-
-    Returns:
-        JSON response with a success message or error details.
-    """
-
-    success = eliminar_noticia(db, noticia_id)
-    if not success:
-        raise HTTPException(status_code=404, detail="Noticia no encontrada")
-    return {"message": "Noticia eliminada correctamente"}
 
 @router.get("/noticias/", response_model=list[NoticiaSchema])
 def read_noticias(db: Session = Depends(get_db)):
