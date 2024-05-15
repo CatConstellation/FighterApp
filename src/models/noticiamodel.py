12bab36c8fc63b5b9fc4b2c23642ffa0d1514b9c
HenryVQ197 <72803163+HenryVQ197@users.noreply.github.com>
2024-05-15 08:02:22 -0500

    Update noticiamodel.py

--- src/models/noticiamodel.py
+++ src/models/noticiamodel.py
@@ -14,17 +14,6 @@ def create_noticia(db: Session, noticia: NoticiaCreate):
     db.refresh(db_noticia)
     return db_noticia
 
-def get_eliminarnoticia():
-    return FileResponse("src/views/eliminarnoticia.html")
-
-def eliminar_noticia(db: Session, noticia_id: int):
-    db_noticia = db.query(Noticia).filter(Noticia.id_noticia == noticia_id).first()
-    if db_noticia:
-        db.delete(db_noticia)
-        db.commit()
-        return True
-    return False
-
 def get_noticias(db: Session):
     return db.query(Noticia).all()
 
