5f5e6064e092ea36ce12d1998a5c35dc2f963fa1
HenryVQ197 <72803163+HenryVQ197@users.noreply.github.com>
2024-05-24 16:36:53 -0500

    Update schemas.py

--- src/models/schemas.py
+++ src/models/schemas.py
@@ -1,21 +1,21 @@
 from pydantic import BaseModel
 from datetime import date
 
-#Actúa como una clase base,reduciendo la duplicación 
-#de código y asegurando que los campos comunes estén centralizados.
+# Actúa como una clase base, reduciendo la duplicación de código y asegurando que los campos comunes estén centralizados.
 class NoticiaBase(BaseModel):
     titulo: str
     cuerpo: str
     archivo: str
     fecha: date
-  
-class NoticiaDelete(BaseModel):
+
+# Extiende NoticiaBase y añade campos específicos necesarios solo para la creación.
+class NoticiaCreate(NoticiaBase):
     id_noticia: int
-  
+
 # Extiende NoticiaBase y añade campos necesarios para la respuesta de la API,
 # junto con configuraciones específicas para la interoperabilidad con SQLAlchemy.
 class NoticiaSchema(NoticiaBase):
     id_noticia: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
